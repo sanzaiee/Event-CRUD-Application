993742ccfe2889e88aa4dc40f939b1ee873217b6
sanzaiee <snjprajapati3@gmail.com>
2022-11-18 08:36:06 +0545

    design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,30 +3,22 @@
 @inject('event_count', 'App\Helpers\SiteHelper')
 
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <div class="card mb-3 welcome-section">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="{{ asset('backend/images/event.jpg') }}" class="img-fluid rounded-start" alt="event">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Events</h5>
+              <p class="card-text">Total Number of events <span class="badge rounded-pill bg-info text-dark">{{ $event_count->eventCount() }}</span></p>
 
-                    <div class="card">
-                        <div class="card-header">
-                          Events
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Total Number of events <span class="badge rounded-pill bg-info text-dark">{{ $event_count->eventCount() }}</span></h5>
-                            <a href="{{ route('events.index') }}" class="btn btn-primary">Events List</a>
-                        </div>
-                      </div>
-                </div>
+              <a href="{{ route('events.index') }}" class="btn btn-primary">Events List</a>
+
             </div>
+          </div>
         </div>
-    </div>
+      </div>
 </div>
 @endsection
